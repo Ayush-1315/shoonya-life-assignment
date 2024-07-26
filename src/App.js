b906cde972fba6f572f777a70c80cd969c36513b
Ayush Raj <ayush.raj138@gmail.com>
2024-07-26 20:29:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes,Route } from "react-router-dom";
+import {Routes,Route} from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/home/home";
 import { RetreatPage } from "./pages/retreatPage/retreatPage";
